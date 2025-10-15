83e8dc52a40ab9713b1f54668bcf1738b7a34851
Sindhuja Parthasarathy <sarathy.sindhu@gmail.com>
2025-10-15 15:14:13 +0200

    Added few more methods in time

--- src/main/java/se/lexicon/TimeExercise.java
+++ src/main/java/se/lexicon/TimeExercise.java
@@ -1,10 +1,7 @@
 package se.lexicon;
 
 import javax.swing.text.DateFormatter;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Month;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -36,6 +33,49 @@ public class TimeExercise {
         Month month=futureDate.getMonth();
         System.out.println("The future Date is:"+futureDate);
         System.out.println("The month is: "+month);
+
+        Period period=Period.between(birthday,futureDate);
+        System.out.println("The Years: "+period.getYears());
+        System.out.println("The Months: "+period.getMonths());
+        System.out.println("The Days: "+period.getDays());
+
+        Period period1=Period.of(4,7,29);
+        LocalDate newDate=currentDate.plus(period1);
+
+        System.out.println("The currentDate is: "+currentDate);
+        System.out.println("The new date after adding period: "+newDate);
+
+        LocalTime currentTime=LocalTime.now();
+        int nanoSecs=currentTime.getNano();
+        System.out.println(nanoSecs);
+
+        LocalTime time=LocalTime.parse("14:44:30");
+        System.out.println("Parsed Time: "+ time);
+
+        DateTimeFormatter timeFormatter =DateTimeFormatter.ofPattern("HH:mm:ss");
+        String formattedTime=currentTime.format(timeFormatter);
+        System.out.println("Formatted time is: "+formattedTime);
+
+        LocalDateTime dateTime=LocalDateTime.of(2018,4,5,10,0);
+        System.out.println("LocalDateTime :"+dateTime);
+
+        DateTimeFormatter timeFormat=DateTimeFormatter.ofPattern("EEEE d MMMM HH:mm", new Locale("sv", "SE"));
+        String formatted=dateTime.format(timeFormat);
+        System.out.println(formatted);
+
+        LocalDate dte=LocalDate.of(2025,10,15);
+        LocalTime tme=LocalTime.of(15,5);
+        LocalDateTime dtTme=LocalDateTime.of(dte,tme);
+        System.out.println("Combined Localdatetime :"+dtTme);
+
+        LocalDateTime dteTime=LocalDateTime.now();
+        LocalDate lclDate = dteTime.toLocalDate();
+        LocalTime lclTime = dteTime.toLocalTime();
+
+        System.out.println("The Current time: "+currentTime);
+        System.out.println("Extracted LocalDate: "+lclDate);
+        System.out.println("Extracted LocalTime: "+lclTime);
+
 
 
     }
